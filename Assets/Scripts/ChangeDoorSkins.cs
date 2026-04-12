9f17bdc1c4d4ad9667f0eae149ef13b9717f9844
luisprado89 <luisdpc89@gmail.com>
2026-04-12 11:27:43 +0200

    feat: añadido fondo, prefabs y scripts básicos del nivel 1
    - Añadidos backgrounds (dos.png, uno.png)
    - Creación de prefabs:
      - TextAlFruitCollected
      - TransitionAnimation
    - Añadida escena Level1
    - Scripts añadidos:
      - ChangeDoorSkins
      - PlayerSelect
    - Configuración inicial del sistema de transición y recogida

---
+++ Assets/Scripts/ChangeDoorSkins.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ChangeDoorSkins : MonoBehaviour
+{
+    public GameObject skinsPanel; // Referencia al panel de skins
+    public bool inDoor = false; // Variable para verificar si el jugador está dentro de la puerta
+    public GameObject player; // Referencia al jugador
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inDoor = true; // El jugador ha entrado en la puerta
+            skinsPanel.gameObject.SetActive(true); // Mostrar el panel de skins
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inDoor = false; // El jugador ha salido de la puerta
+            skinsPanel.gameObject.SetActive(false); // Ocultar el panel de skins
+        }
+    }
+    public void SetPlayerMat()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "Mat"); // Guardar la selección del jugador en PlayerPrefs
+        ResetPlayerSkin(); // Reiniciar la apariencia del jugador
+    }
+    public void SetPlayerDino()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "Dino"); // Guardar la selección del jugador en PlayerPrefs
+        ResetPlayerSkin(); // Reiniciar la apariencia del jugador
+    }
+    void ResetPlayerSkin()
+    {
+        skinsPanel.gameObject.SetActive(false); // Ocultar el panel de skins
+        player.GetComponent<PlayerSelect>().ChangePlayerInMenu(); // Llamar al método para cambiar la apariencia del jugador en el menú
+    }
+}
--- Assets/Scripts/PlayerSelect.cs
+++ Assets/Scripts/PlayerSelect.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerSelect : MonoBehaviour
 {
+    public bool enableSelectCharacter; // Variable para habilitar la selección de personaje
     public enum Player { Mat, Dino }// Para seleccionar el personaje
     public Player playerSelected; // Variable para almacenar el personaje seleccionado
     public Animator animator; // Referencia al componente Animator
@@ -10,13 +11,37 @@ public class PlayerSelect : MonoBehaviour
     public Sprite[] playersRenderer; // Array de sprites para cada personaje
     void Start()
     {
-        switch (playerSelected)
+        if (!enableSelectCharacter) // Si la selección de personaje no está habilitada, asignamos el personaje por defecto
         {
-            case Player.Mat: // Si el personaje seleccionado es Mat, asignamos su controlador de animación y sprite
+            ChangePlayerInMenu(); // Llamar al método para cambiar la apariencia del jugador en el menú
+        }
+        else
+        {
+
+            switch (playerSelected)
+            {
+                case Player.Mat: // Si el personaje seleccionado es Mat, asignamos su controlador de animación y sprite
+                    animator.runtimeAnimatorController = playersControllers[0];
+                    spriteRenderer.sprite = playersRenderer[0];
+                    break;
+                case Player.Dino: // Si el personaje seleccionado es Dino, asignamos su controlador de animación y sprite
+                    animator.runtimeAnimatorController = playersControllers[1];
+                    spriteRenderer.sprite = playersRenderer[1];
+                    break;
+            }
+        }
+    }
+
+    public void ChangePlayerInMenu() // Método para cambiar la apariencia del jugador en el menú
+    {
+
+        switch (PlayerPrefs.GetString("PlayerSelected")) // Obtener el personaje seleccionado de PlayerPrefs
+        {
+            case "Mat": // Si el personaje seleccionado es Mat, asignamos su controlador de animación y sprite
                 animator.runtimeAnimatorController = playersControllers[0];
                 spriteRenderer.sprite = playersRenderer[0];
                 break;
-            case Player.Dino: // Si el personaje seleccionado es Dino, asignamos su controlador de animación y sprite
+            case "Dino": // Si el personaje seleccionado es Dino, asignamos su controlador de animación y sprite
                 animator.runtimeAnimatorController = playersControllers[1];
                 spriteRenderer.sprite = playersRenderer[1];
                 break;
